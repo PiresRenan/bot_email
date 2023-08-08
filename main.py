789585fc281c85485660d1fa0ef745f55f01c3f0
Renan Pires <renan.sp.121@hotmail.com>
2023-08-08 16:02:31 -0300

    [08/08/2023]

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+async def root():
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
