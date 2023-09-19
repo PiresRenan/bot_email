3004ac9fc74659b426deec3b5ad01afc2a9dc6a3
Renan Pires <renan.sp.121@hotmail.com>
2023-09-19 17:27:18 -0300

    [19/09/2023]

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ async def index():
 @app.get("/on")
 async def index():
     global tasks
-    INTERVALO = 5
+    INTERVALO = 60
     # INTERVALO = 10
     fuso_horario_brasilia = pytz.timezone('America/Sao_Paulo')
     if not tasks:
